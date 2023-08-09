17d3404552ad58575050dc91ad9f22ac0c243e67
itsAdee <adeelahmedqureshi02@gmail.com>
2023-08-09 18:32:29 +0500

    Added a ServiceController Class in Controller Paclage

---
+++ Java_Utility/src/Controller/ServiceController.java
@@ -0,0 +1,95 @@
+package Controller;
+
+import Model.ServiceType;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServiceController {
+
+    private static final String PRICES_FILE_PATH = "src\\Model\\ServicePrices.txt";
+
+    private static Map<ServiceType, Double[]> servicePrices = new HashMap<>();
+
+    static {
+        loadPricesFromFile();
+    }
+
+    private static void loadPricesFromFile() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(PRICES_FILE_PATH))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(":");
+                if (parts.length == 2) {
+                    String serviceName = parts[0];
+                    String[] prices = parts[1].split(",");
+                    if (prices.length == 2) {
+                        Double unitPrice = Double.parseDouble(prices[0]);
+                        Double servicePrice = Double.parseDouble(prices[1]);
+                        ServiceType serviceType = ServiceType.valueOf(serviceName);
+                        servicePrices.put(serviceType, new Double[] { unitPrice, servicePrice });
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void savePricesToFile() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(PRICES_FILE_PATH))) {
+            for (Map.Entry<ServiceType, Double[]> entry : servicePrices.entrySet()) {
+                ServiceType serviceType = entry.getKey();
+                Double[] prices = entry.getValue();
+                writer.write(serviceType.name() + ":" + prices[0] + "," + prices[1]);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void updateUnitCharges(ServiceType serviceType, double newUnitCharges) {
+        Double[] prices = servicePrices.getOrDefault(serviceType, new Double[] { 0.0, 0.0 });
+        prices[0] = newUnitCharges;
+        servicePrices.put(serviceType, prices);
+        savePricesToFile();
+    }
+
+    public static void updateServiceCharges(ServiceType serviceType, double newServiceCharges) {
+        Double[] prices = servicePrices.getOrDefault(serviceType, new Double[] { 0.0, 0.0 });
+        prices[1] = newServiceCharges;
+        servicePrices.put(serviceType, prices);
+        savePricesToFile();
+    }
+
+    public static double getServicePrice(ServiceType serviceType) {
+        Double[] prices = servicePrices.getOrDefault(serviceType, new Double[] { 0.0, 0.0 });
+        return prices[1]; // Return the service price
+    }
+
+    public static double getUnitPrice(ServiceType serviceType) {
+        Double[] prices = servicePrices.getOrDefault(serviceType, new Double[] { 0.0, 0.0 });
+        return prices[0]; // Return the unit price
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        ServiceType electricity = ServiceType.ELECTRICITY;
+
+        System.out.println("Before Update");
+
+        System.out.println(getUnitPrice(electricity));
+        System.out.println(getServicePrice(electricity));
+
+        updateUnitCharges(electricity, 0.15);
+        updateServiceCharges(electricity, 2.0);
+
+        System.out.println("After update");
+        // Print updated charges
+        System.out.println(getUnitPrice(electricity));
+        System.out.println(getServicePrice(electricity));
+
+    }
+}
